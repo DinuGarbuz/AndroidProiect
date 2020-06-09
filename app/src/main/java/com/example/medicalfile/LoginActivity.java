6acae7c232c68d22d3a128682cc05da16d1d3188
DinuGarbuz <DinuGarbuz@users.noreply.github.com>
2020-06-09 22:21:11 +0300

    Finish login and registration.

--- app/src/main/java/com/example/medicalfile/LoginActivity.java
+++ app/src/main/java/com/example/medicalfile/LoginActivity.java
@@ -8,18 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText mTextUsername;
     EditText mTextPassword;
     Button mButtonLogin;
     TextView mTextViewRegister;
+    DatabaseHelper db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new DatabaseHelper(this);
         mTextUsername = (EditText)findViewById(R.id.edittext_username);
         mTextPassword = (EditText)findViewById(R.id.edittext_password);
         mButtonLogin = (Button) findViewById(R.id.button_login);
@@ -28,9 +32,27 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+
                 startActivity(registerIntent);
             }
         });
+       mButtonLogin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String user = mTextUsername.getText().toString().trim();
+               String pwd = mTextPassword.getText().toString().trim();
+               Boolean res = db.checkUser(user, pwd);
+               if(res == true)
+               {
+                  Intent HomePage = new Intent(LoginActivity.this, HomeActivity.class);
+                  startActivity(HomePage);
+               }
+               else
+               {
+                   Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+               }
+           }
+       });
 
     }
 }
