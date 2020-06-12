65f02977c037568b03a42f32edc7e88310a54485
lazardragosiulian <lazar.dragos.iulian@gmail.com>
2020-06-12 22:16:16 +0300

    adaugat meniu cu cate 2 butoane la client/medic + 1 fragment la client

--- app/src/main/java/com/example/medicalfile/ClientActivity.java
+++ app/src/main/java/com/example/medicalfile/ClientActivity.java
@@ -1,14 +1,43 @@
 package com.example.medicalfile;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ClientActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+
     }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_fisa_medicala, container, false);
+
+        b1 = v.findViewById(R.id.button1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(ClientActivity.this,FisaMedicala.class));
+        }
+        });
+
+        return v;
+    }
+
+
 }
