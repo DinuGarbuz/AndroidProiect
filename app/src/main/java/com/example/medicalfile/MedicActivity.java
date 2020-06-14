f59161c5e65ae493f407b1cc20856e66eba4e382
lorenalorri <62277601+lorenalorri@users.noreply.github.com>
2020-06-14 21:36:28 +0300

    adding medic fragment and put everything in it

--- app/src/main/java/com/example/medicalfile/MedicActivity.java
+++ app/src/main/java/com/example/medicalfile/MedicActivity.java
@@ -1,6 +1,7 @@
 package com.example.medicalfile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class MedicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medic);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new MedicFragment());
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/medicalfile/MedicFragment.java
@@ -0,0 +1,22 @@
+package com.example.medicalfile;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class MedicFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_medic, container, false);
+
+        return view;
+    }
+}
