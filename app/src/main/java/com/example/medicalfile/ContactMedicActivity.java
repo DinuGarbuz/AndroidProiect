73cc2937a8f086ec593a3500494e07e537bea722
lazardragosiulian <lazar.dragos.iulian@gmail.com>
2020-06-17 21:14:03 +0300

    implementat contacte medici

--- app/src/main/java/com/example/medicalfile/ContactMedicActivity.java
+++ app/src/main/java/com/example/medicalfile/ContactMedicActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,18 +20,27 @@ public class ContactMedicActivity extends AppCompatActivity {
     DatabaseHelper db ;
 
     private ListView mListView;
+
+    int[] images={R.drawable.medic1,R.drawable.medic2,R.drawable.medic3,R.drawable.medic4,R.drawable.medic5,};
+    String[] names={"Dan Diaconescu", "lica Samadau", "Hdolf Aitler", "Ceausescu", "Stela Popescu"};
+    String[] numbers={"1234567890", "072547392", "214577642", "1245678765","086124555"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fisa);
 
         mListView = (ListView)findViewById(R.id.listView);
-        db = new DatabaseHelper(this);
+        //db = new DatabaseHelper(this);
 
-        populateListView();
+        //  populateListView();
 
+
+        CustomAdaptor customAdaptor = new CustomAdaptor();
+        mListView.setAdapter(customAdaptor);
     }
-
+/*
     private void populateListView()
     {
 
@@ -41,7 +55,38 @@ public class ContactMedicActivity extends AppCompatActivity {
         }
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         mListView.setAdapter( adapter);
+    }
+*/
 
+    class CustomAdaptor extends BaseAdapter{
 
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            View v=getLayoutInflater().inflate(R.layout.customlayout,null);
+
+            ImageView mPoza = v.findViewById(R.id.imageView);
+            TextView mNume = v.findViewById(R.id.nume);
+            TextView mNrTel = v.findViewById(R.id.nrTelefon);
+
+            mPoza.setImageResource(images[position]);
+            mNume.setText(names[position]);
+            mNrTel.setText(numbers[position]);
+            return v;
+        }
     }
 }
