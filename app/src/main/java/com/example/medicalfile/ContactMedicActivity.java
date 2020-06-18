268e5367471d47eb8d96339e4cc235f4b3dcf944
DinuGarbuz <DinuGarbuz@users.noreply.github.com>
2020-06-18 09:41:38 +0300

    Add medic from db.

--- app/src/main/java/com/example/medicalfile/ContactMedicActivity.java
+++ app/src/main/java/com/example/medicalfile/ContactMedicActivity.java
@@ -22,8 +22,10 @@ public class ContactMedicActivity extends AppCompatActivity {
     private ListView mListView;
 
     int[] images={R.drawable.medic1,R.drawable.medic2,R.drawable.medic3,R.drawable.medic4,R.drawable.medic5,};
-    String[] names={"Dan Diaconescu", "lica Samadau", "Hdolf Aitler", "Ceausescu", "Stela Popescu"};
-    String[] numbers={"1234567890", "072547392", "214577642", "1245678765","086124555"};
+    String[] names;//={"Dan Diaconescu", "lica Samadau", "Hdolf Aitler", "Ceausescu", "Stela Popescu"};
+    String[] numbers;//={"1234567890", "072547392", "214577642", "1245678765","086124555"};
+//ArrayList<String> names = new ArrayList<String>();
+//    ArrayList<String> numbers = new ArrayList<String>();
 
 
     @Override
@@ -32,31 +34,41 @@ public class ContactMedicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fisa);
 
         mListView = (ListView)findViewById(R.id.listView);
-        //db = new DatabaseHelper(this);
+        db = new DatabaseHelper(this);
 
-        //  populateListView();
+        //populateListView(names, numbers);
 
 
         CustomAdaptor customAdaptor = new CustomAdaptor();
         mListView.setAdapter(customAdaptor);
     }
-/*
-    private void populateListView()
-    {
 
-        Cursor data = db.getMedic();
-        ArrayList<String> listData = new ArrayList<>();
-        while(data.moveToNext())
-        {
-            String aux = "";
-            aux = data.getString(1) + " " + data.getString(2) + " - "+ data.getString(5);
+//    private void populateListView(String[] names, String[] numbers)
+//    {
+//
+//        Cursor data = db.getMedic();
+//        int i =0;
+//      //  ArrayList<String> listData = new ArrayList<>();
+//        while(data.moveToNext())
+//        {
+//
+////            String aux = "";
+////            aux = data.getString(1) + " " + data.getString(2) + " - "+ data.getString(5);
+////
+////            listData.add(aux);
+//            names[i] = data.getString(1);
+//            numbers[i] =data.getString(5);
+//            i++;
+//
+//        }
+//        names[3]= "asdads";
+//        names[4]= "asaaaa";
+//        numbers[3] = "123132";
+//        numbers[4]= "1111";
+////        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+////        mListView.setAdapter( adapter);
+//    }
 
-            listData.add(aux);
-        }
-        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
-        mListView.setAdapter( adapter);
-    }
-*/
 
     class CustomAdaptor extends BaseAdapter{
 
@@ -78,14 +90,31 @@ public class ContactMedicActivity extends AppCompatActivity {
         @Override
         public View getView(int position, View view, ViewGroup viewGroup) {
             View v=getLayoutInflater().inflate(R.layout.customlayout,null);
+            Cursor data = db.getMedic();
+
+            ArrayList<String> names = new ArrayList<String>();
+            ArrayList<String> numbers = new ArrayList<String>();
 
             ImageView mPoza = v.findViewById(R.id.imageView);
             TextView mNume = v.findViewById(R.id.nume);
             TextView mNrTel = v.findViewById(R.id.nrTelefon);
 
             mPoza.setImageResource(images[position]);
-            mNume.setText(names[position]);
-            mNrTel.setText(numbers[position]);
+
+            while(data.moveToNext()) {
+
+                names.add(data.getString(1));
+                numbers.add(data.getString(5));
+            }
+            names.add("asd");
+            names.add("asdaaa");
+            numbers.add("1111");
+            numbers.add("2222");
+
+                mNume.setText(names.get(position));
+                mNrTel.setText(numbers.get(position));
+
+
             return v;
         }
     }
--- app/src/main/java/com/example/medicalfile/LoginActivity.java
+++ app/src/main/java/com/example/medicalfile/LoginActivity.java
@@ -2,10 +2,12 @@ package com.example.medicalfile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,8 +19,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -29,6 +36,49 @@ public class LoginActivity extends AppCompatActivity {
     DatabaseHelper db;
     CheckBox checkBox;
 
+
+
+
+//    private void writeToFile(String data,Context context) {
+//        try {
+//            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("user.txt", Context.MODE_PRIVATE));
+//            outputStreamWriter.write(data);
+//            outputStreamWriter.close();
+//        }
+//        catch (IOException e) {
+//            Log.e("Exception", "File write failed: " + e.toString());
+//        }
+//    }
+//
+//    private String readFromFile(Context context) {
+//
+//        String ret = "";
+//
+//        try {
+//            InputStream inputStream = context.openFileInput("user.txt");
+//
+//            if ( inputStream != null ) {
+//                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+//                String receiveString = "";
+//                StringBuilder stringBuilder = new StringBuilder();
+//
+//                while ( (receiveString = bufferedReader.readLine()) != null ) {
+//                    stringBuilder.append("\n").append(receiveString);
+//                }
+//
+//                inputStream.close();
+//                ret = stringBuilder.toString();
+//            }
+//        }
+//        catch (FileNotFoundException e) {
+//            Log.e("login activity", "File not found: " + e.toString());
+//        } catch (IOException e) {
+//            Log.e("login activity", "Can not read file: " + e.toString());
+//        }
+//
+//        return ret;
+//    }
 
 
     @Override
@@ -51,6 +101,9 @@ public class LoginActivity extends AppCompatActivity {
         mTextViewRegister = (TextView) findViewById(R.id.textview_register);
         checkBox = (CheckBox)findViewById(R.id.checkbox_password);
         mTextPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+
+
 
 
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -82,16 +135,7 @@ public class LoginActivity extends AppCompatActivity {
               Boolean res = db.checkUser(mail, pwd);
                if(res == true)
                {
-//                   File f = new File("current_user.txt");
-//
-//                   if(f.exists()){
-//                       f.delete();
-//                       try {
-//                           f.createNewFile();
-//                       } catch (IOException e) {
-//                           e.printStackTrace();
-//                       }
-//                   }
+
 
 
                    if (MySpinner.getSelectedItem().toString().equals("Medic"))
